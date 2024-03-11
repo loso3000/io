be08750192e36e31abe8a4465cb80936b1949222
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2024-03-12 07:58:57 +0800

    1

--- .github/tmp/common/tools.sh
+++ .github/tmp/common/tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $OP == amd64 ] ;then
+if [ $1 == amd64 ] ;then
 BASE_FILES=${GITHUB_WORKSPACE}/openwrt/package/base-files/files
 			singbox_version="1.8.5"
 			hysteria_version="2.2.4"
