4ae9b0f22c741cab6699158a1e5ce655d9017a24
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2024-12-01 10:16:26 +0800

    x

--- .github/tmp/common/tools.sh
+++ .github/tmp/common/tools.sh
@@ -22,3 +22,20 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/p
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
 popd
+
+## opkg ##
+PLATFORM=$(cat .config | grep CONFIG_TARGET_ARCH_PACKAGES | awk -F '"' '{print $2}')
+TARGET=$(cat .config | grep CONFIG_TARGET_BOARD | awk -F '"' '{print $2}')
+SUBTARGET=$(cat .config | grep CONFIG_TARGET_SUBTARGET | awk -F '"' '{print $2}')
+
+mkdir -p files/etc/opkg
+pushd files/etc/opkg
+cat <<-EOF > "distfeeds.conf"
+src/gz openwrt_core https://mirrors.cloud.tencent.com/snapshots/targets/$TARGET/$SUBTARGET/packages
+src/gz openwrt_base https://mirrors.cloud.tencent.com/snapshots/packages/$PLATFORM/base
+src/gz openwrt_luci https://mirrors.cloud.tencent.com/snapshots/packages/$PLATFORM/luci
+src/gz openwrt_packages https://mirrors.cloud.tencent.com/snapshots/packages/$PLATFORM/packages
+src/gz openwrt_routing https://mirrors.cloud.tencent.com/snapshots/packages/$PLATFORM/routing
+src/gz openwrt_telephony https://mirrors.cloud.tencent.com/snapshots/packages/$PLATFORM/telephony
+EOF
+popd
